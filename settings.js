a3a65d9396daf29994a07d3864aa7293d2cd8e1a
sunepa22 <dpay491@gmail.com>
2025-08-27 10:55:41 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "Knight Bot",
   botOwner: 'Sunepa, // Your name
-  ownerNumber: '23452369671', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '2349052369671', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
